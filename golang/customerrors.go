6e5c0b8055a0251681cee99ed246818f7f5919b1
tim48-robot <justintimothy303@gmail.com>
2026-06-19 07:17:01 +0700

    here we learn more about customerrors, i see in the tutorial that there is a errors.AsType but i cant access that function, seems version incompatibility? anyways i used errors.As which uses error interface & the real error type for the input, if return true then it is true. Well as for the data itself is being injected straight into the second input/parameter which is supposedly a variable? that has a datatype of the real error type of course. different than errors.AsType where you check then get the value & take it by 2 var which is value, ok for example. if ok false, value is nil? since its type supposed to be *argError, befroe unboxing, of course that the syntax is quite different errors.AsType[*argError](err)

---
+++ golang/customerrors.go
@@ -0,0 +1,53 @@
+/*
+data type of struct is that we define
+data type of interface is that we would normally use the function that is the preconditon of being that interface
+in this case, 
+type error interface {
+	Error() string
+}
+
+anyways im going to make a custom error using that stuff?
+*/
+package main
+import (
+	"fmt"
+	"errors"
+)
+
+type argError struct{
+	data int
+	argument string
+}
+
+// here i dont understand why we need pointer while copy is safer, altho maybe overhead copy will not take much memory
+// but i guess go convention is just like that 
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s",e.data, e.argument)
+}
+
+func divide(a int, b int)(int, error){
+	if b == 0 {
+		return -1, &argError{b, "cannot divide by zero"}
+	} else {
+		return a/b, nil
+	}
+}
+
+func main(){
+	_, err := divide(1,5) // err type is error btw, since return of divide is error
+	// thats why gotta open the interface using the errors.AsType
+	var ae *argError
+	if errors.As(err, &ae){
+		fmt.Println(ae.data, ae.argument)
+	} else {
+		fmt.Printf("This is not an argError")
+		fmt.Println()
+	}
+
+	_, err = divide(1,0)
+	if errors.As(err, &ae){
+		fmt.Println(ae.data, ae.argument)
+	} else {
+		fmt.Printf("This is not an argError")
+	}
+}
